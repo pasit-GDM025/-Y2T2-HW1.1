a9a32f3e542b76e537806095946dbe2eca5651d4
pasit-GDM025 <pasit.pipat@mail.kmutt.ac.th>
2023-02-05 11:17:52 +0700

    Done

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program{
+    public static void Main(string[]args){
+        double Input, ans;
+        Console.WriteLine("Input number:");
+        Input = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ans:");
+        Console.WriteLine(Cal(Input));
+}
+    public static double Cal(double Input)
+    {
+       if(Math.Pow(Input,2)<4 || Math.Pow(Input,2)==4){
+         return 2;
+       }
+       else{
+         double ans = 1+Cal(Input - (4*Math.Abs(Input)/Input));
+         return ans;
+       }
+    }
+}
